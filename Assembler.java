fe8f7e0bdb33fe00428db447db27d63ebf3d0c31
bmyers17 <bmyers2017@gmail.com>
2017-04-10 12:28:53 -0500

    Major progress
    Added variable and macro decoding as well as overall structure for assembly. Still need to update assembler register loading information for better efficiency.

--- Assembler.java
+++ Assembler.java
@@ -9,19 +9,29 @@ public class Assembler
 	private static final char COMMENT = '#';
 	private static final String LABEL = "label";
 	private static final String CURRENT = "here";
+	private static final char REGISTER = '@';
+	private static final char ARGUMENT = '&';
+	private static final String ASSEMBLER = "@assembler";
+	private static final String LOAD = "LOD";
+	private static final String L = 'L';
+	private static final String machineFile = "machine.txt";
+	private static final String macroFile = "macros.txt";
+	
+	private static List<String> supported;
+	private static HashMap<String, String> variables;
+	private static HashMap<String[], String[][]> macros;
+	
+	private static int id = 0;
 	
 	private static String[] assemble(String[] initFile)
 	{
 		initFile = clean(initFile);
 		String[][] file = tokenize(initFile);
 		
-		HashMap<String, String> variables = new HashMap<String, String>();
+		variables = new HashMap<String, String>();
 		file = stripVars(file, variables);
-	}
-	
-	private static String[] unpack(String[][] file)
-	{
 		
+		return interpret(prepare(file));
 	}
 
 	private static String[] clean(String[] file)
@@ -77,7 +87,119 @@ public class Assembler
 		return stripped.toArray(new String[stripped.size()][]);
 	}
 	
-	private static boolean machineFriendly(String[] instruction)
+	private static String[] prepare(String[][] file)
 	{
+		supported = new TreeSet<String>(IOManager.read(machineFile));
+		
+		List<String[]> packed = new LinkedList<String[]>(file);
+		List<String[]> unpacked = new LinkedList<String[]>();
+		List<String[]> machineReady = new LinkedList<String[]>();
+		
+		resolveMacros(packed, unpacked);
+		resolveVariables(unpacked);
+		resolveMachine(unpacked, machineReady);
+		resolveVariables(machineReady);
+		
+		while (!packed.isEmpty())
+			unpackLine(packed.remove(0), packed, unpacked, supported);
+		
+		return unpacked.toArray(new String[unpacked.size()][]);
+	}
+			      
+	private static void resolveMacros(List<String[]> packed, List<String[]> unpacked)
+	{
+		if (packed.isEmpty())
+			return unpacked;
+		
+		line = packed.remove(0);
+		
+		if (!macros.containsKey(line[0]))
+			unpacked.add(line);
+		else
+		{
+			for (int k = 1; k < line.length; k++)
+				variables.put(ARGUMENT + "" + k + "_" + id, line[k]);
+			
+			String[][] macro = copy(macros.get(line[0]));
+			
+			for (int k = 0; k < macro.length; k++)
+				for (int j = 0; j < macro[k].length; j++)
+					if (macro[k][j].charAt(0) = ARGUMENT)
+						macro[k][j] += "_" + id;
+			id++;
+			
+			for (int k = macro.length - 1; k >= 0; k--)
+				packed.add(macro[k], 0);
+		}
+		
+		resolveMacros(packed, unpacked);
+	}
+
+	
+	private static void resolveVariables(List<String[]> file)
+	{
+		for (String[] line : file)
+			for (int k = 1; k < line.length; k++)
+				if (variables.containsKey(line[k]))
+					line[k] = variables.get(line[k]);
+	}
+	
+	private static void resolveMachine(List<String[]> unpacked, List<String[]> ready)
+	{
+		fixNumbers(unpacked);
+		addLoads(unpacked, ready);
+	}
+	
+	private static void fixNumbers(List<String[]> unpacked)
+	{
+		for (String[] line : unpacked)
+			for (int k = 1; k < line.length; k++)
+				line[k] = convertBase(line[k]);
+	}
+	
+	private static void addLoads(List<String[]> unpacked, List<String[]> ready)
+	{
+		for (String[] line : unpacked)
+		{
+			if (!line[0].equals(load) && line[1].charAt(0) == L)
+			{
+				ready.add(new String[2] {LOAD, ASSEMBLER, line[1].substring(1)});
+				line[1] = ASSEMBLER;
+			}
+			
+			ready.add(line);
+		}	
+	}
+	
+	private static String convertBase(String number)
+	{
+		char base = number.charAt(0);
+		String rest = number.substring(1);
+		int value = -1;
+		
+		if (base == 'x')
+			value = Integer.parseInt(rest, 16);
+		else if (base == 'd')
+			value = Integer.parseInt(rest, 10);
+		else if (base == 'b')
+			value = Integer.parseInt(rest, 2);
+		else
+			return number;
+		
+		return L + Integer.toString(value, 2);
+	}
+			      
+	private static String[][] copy(String[][] arr)
+	{
+		String[][] copied = new String[arr.length][];
+		
+		for (int r = 0; r < arr.length; r++)
+		{
+			copied[r] = new String[arr[r].length];
+			for (int c = 0; c < arr[r].length; c++)
+				copied[r][c] = arr[r][c];
+		}
+		
+		return copied;
 	}
 }
